4587a0ec834e70ee1ee772eade153bca9afc60b6
Vincent Siu <vince.siu4@gmail.com>
2015-09-29 11:23:04 -0700

    Attempted to fix the error where arguments are not used in execute_command()

--- lab1a/execute-command.c
+++ lab1a/execute-command.c
@@ -20,5 +20,11 @@ execute_command (command_t c, int time_travel)
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
-  error (1, 0, "command execution not yet implemented");
+
+     c->status++;
+     c->status--;
+     time_travel++;
+
+
+  // error (1, 0, "command execution not yet implemented");
 }
