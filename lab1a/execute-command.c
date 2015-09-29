f46d6ccae1061f9bbd00cf021d29859ec17cc714
vince <siuv@lnxsrv02.seas.ucla.edu>
2015-09-29 11:34:40 -0700

    make command successfully compiles now.

--- lab1a/execute-command.c
+++ lab1a/execute-command.c
@@ -21,10 +21,8 @@ execute_command (command_t c, int time_travel)
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
 
-     c->status++;
-     c->status--;
-     time_travel++;
-
+	c;
+	time_travel;
 
   // error (1, 0, "command execution not yet implemented");
 }
