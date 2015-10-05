516e0eedfb24e1a1f4e4c025bfaf0f95d862f87b
vince <siuv@lnxsrv04.seas.ucla.edu>
2015-10-05 02:10:17 -0700

    Finished the subtokenizer part, now will work on the tokenizer part, which does rudimentary error checking

--- lab1a/tokenizer.c
+++ lab1a/tokenizer.c
@@ -1,53 +1,153 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/*
-int isValidOperator(struct Token *foo) {
-	if (foo->len == 1) {
-		switch(foo->word[0]) {
-			case '|': 
-			case ';':
-			case '(':
-			case ')':
-			case '<':
-			case '>':
-				return true;
-			default: 
-				return false;
-		}
-	}
-	if (foo->len == 2) {
-		if (foo->word[0] == '&' && foo->word[1] == '&')
-			return true;
-		if (foo->word[1] == '|' && foo->word[1] == '|')
-			return true;
-	}
-	
-	return false;
-	
-}
+#include "tokenizer.h"
 
-int isValidWord() {
-
-}
-*/
-
-
-
-void tokenize(char *word) 
+void tokenize(const char *word) 
 {
+  subtoken *head = subtoken_init();
+  subtoken *cur_subtoken = head; 
+  subtoken *old_subtoken = head;
+
+  word = "a|||b";
+
   int idx = 0;
-  struct Token cur = malloc(sizeof(struct Token)); 
-  for (; word[idx] != '\0'; idx++)
+  char cur_char;
+
+  while (word[idx] != '\0')
   {
-   1 = 1;
+
+    cur_char = word[idx];
+
+    if (cur_subtoken->type == S_NULLTOKEN)
+    {
+      if (isOperatorChar(cur_char))
+      {
+        switch(cur_char)
+        {
+          case '(':
+            cur_subtoken->type = S_SUBSHELLLEFT;
+            break;
+          case ')':
+            cur_subtoken->type = S_SUBSHELLRIGHT;
+            break;
+          case ';':
+            cur_subtoken->type = S_SEQUENCE;
+            break;
+          case '>':
+            cur_subtoken->type = S_OUTPUT;
+            break;
+          case '<':
+            cur_subtoken->type = S_INPUT;
+            break;
+          case '|':
+            cur_subtoken->type = S_INCOMPLETEOR;
+            break;
+          case '&':
+            cur_subtoken->type = S_INCOMPLETEAND;
+            break;
+        }
+
+        if (cur_char != '|' && cur_char != '&')
+        {
+          cur_subtoken = subtoken_addNew(cur_subtoken);
+        }
+      }
+      else if (isCommandChar(cur_char))
+      {
+        cur_subtoken->type = S_COMMAND;
+        cur_subtoken->length = 1;
+        cur_subtoken->word = malloc(sizeof(char) * 2);
+        *cur_subtoken->word = cur_char;
+      }
+      else if (cur_char == '\n')
+      {
+        cur_subtoken->type = S_NEWLINE;
+        cur_subtoken = subtoken_addNew(cur_subtoken);
+      }
+      else if (cur_char != ' ' && cur_char != '\t')
+      {
+        printf("error: invalid character\n");
+        abort();
+      }
+
+      idx++;
+    }
+
+
+
+
+
+    else if (cur_subtoken->type == S_INCOMPLETEOR)
+    {
+      if (cur_char == '|')
+      {
+        cur_subtoken->type = S_OR;
+        idx++;
+      }
+      else
+      {
+        cur_subtoken->type = S_PIPE; 
+      }
+      cur_subtoken = subtoken_addNew(cur_subtoken);
+    }
+
+
+
+
+    else if (cur_subtoken->type == S_INCOMPLETEAND)
+    {
+      if (cur_char == '&')
+      {
+        cur_subtoken->type == S_AND;
+        idx++;
+        cur_subtoken = subtoken_addNew(cur_subtoken);
+      }
+      else
+      {
+        printf("error: invalid character");
+        abort();
+      }
+    }
+
+    else if (cur_subtoken->type == S_COMMAND)
+    {
+      if (!isCommandChar(cur_char))
+      {
+        cur_subtoken = subtoken_addNew(cur_subtoken); 
+      }
+      else
+      {
+        cur_subtoken->word[cur_subtoken->length] = cur_char;
+        cur_subtoken->length++;
+        cur_subtoken->word = realloc(cur_subtoken->word, sizeof(char) * (cur_subtoken->length + 1));
+        idx++;
+      }
+    }
+
+
+
+
   }
+
+
+  if (cur_subtoken->type == S_INCOMPLETEOR || cur_subtoken->type == S_INCOMPLETEAND) 
+    abort();
+
+  subtoken_debug(head);
 
 }
 
 int main (int argc, char const *argv[])
 {
+  if (argc == 2)
     tokenize(argv[1]);
-    return 0;
+  else
+  {
+    printf("Please provide one argument to the script.");
+    abort();
+  }
+  return 0;
 }
 
 
@@ -62,9 +162,9 @@ int isOperatorChar(char c)
     case '<':
     case '>':
     case '&':
-        return TRUE;
+      return true;
     default:
-        return FALSE;
+      return false;
   }
 }
 
@@ -91,4 +191,75 @@ int isCommandChar(char c)
     default:
       return false;
   }
+}
+
+subtoken *subtoken_init()
+{
+  subtoken *foo = malloc(sizeof(subtoken));
+
+  foo->length = 0;
+  foo->word = NULL;
+  foo->next = NULL;
+  foo->type = S_NULLTOKEN;
+
+  return foo;
+}
+
+
+void subtoken_debug(subtoken *head)
+{
+  printf("DEBUGGING THE SUBTOKEN STREAM:\n");
+  while (head != NULL)
+  {
+    switch(head->type)
+    {
+      case S_NULLTOKEN:
+        printf("NULLTOKEN\n");
+        break;
+      case S_NEWLINE:
+        printf("NEWLINE\n");
+        break;
+      case S_AND:
+        printf("AND\n");
+        break;
+      case S_OR:
+        printf("OR\n");
+        break;
+      case S_PIPE:
+        printf("PIPE\n");
+        break;
+      case S_INPUT:
+        printf("INPUT\n");
+        break;
+      case S_OUTPUT:
+        printf("OUTPUT\n");
+        break;
+      case S_SUBSHELLLEFT:
+        printf("SUBSHELLLEFT\n");
+        break;
+      case S_SUBSHELLRIGHT:
+        printf("SUBSHELLRIGHT\n");
+        break;
+      case S_INCOMPLETEOR:
+        printf("INCOMPLETEOR\n");
+        break;
+      case S_INCOMPLETEAND:
+        printf("INCOMPLETEAND\n");
+        break;
+      case S_COMMAND:
+        printf("COMMAND\n");
+        break;
+      default:
+        printf("type code: %i\n", head->type);
+        printf("Error, invalid token in final stream\n");
+        abort();
+    }
+    head = head->next;
+  }
+}
+
+subtoken *subtoken_addNew(subtoken *head)
+{
+  head->next = subtoken_init(); 
+  return head->next;
 }
--- lab1a/tokenizer.h
+++ lab1a/tokenizer.h
@@ -1,25 +1,67 @@
 #define true 1
 #define false 0
 
-enum token_type 
-  {
-    COMMAND, 
-    OPERATOR, 
-    OPERATORINCOMPLETE, 
-    UNLOADED,
-  }; 
+enum subtoken_type {
+  S_NULLTOKEN,
+  S_NEWLINE,
+  S_SEQUENCE,
+  S_AND,
+  S_OR,
+  S_PIPE,
+  S_INPUT,
+  S_OUTPUT,
+  S_SUBSHELLLEFT,
+  S_SUBSHELLRIGHT,
+  S_INCOMPLETEOR,
+  S_INCOMPLETEAND,
+  S_COMMAND,
+};
 
-struct Token 
+typedef struct Subtoken
 {
+  enum subtoken_type type;
   char *word;
-  int len;
-  int line_num;
-  struct Token *next;
+  int length;
+  struct Subtoken *next;
+} subtoken;
+
+
+
+
+
+enum token_type {
+  STARTNEWCOMMAND,
+  AND,
+  SEQUENCE,
+  OR,
+  PIPE,
+  SIMPLE,
+  INPUT,
+  OUTPUT,
+  SUBSHELLLEFT,
+  SUBSHELLRIGHT,
+};
+
+typedef struct Token
+{
   enum token_type type;
-}
+  char *word;
+  int length;
+  struct Token *next;
+} token;
 
 int isOperatorChar(char c);
 int isCommandChar(char c);
+
+
+subtoken *subtoken_init();
+subtoken *subtoken_addNew(subtoken *cur_subtoken);
+subtoken *subtoken_nullTokenHandle(char c);
+void subtoken_debug(subtoken *head);
+
+
+/*
 struct Token *token_init();
 void token_setType(enum token_type type);
 void token_addChar(char c);
+*/
