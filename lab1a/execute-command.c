da64bc266f220ad85ea8960fe7f35c4412756cb4
vince <siuv@lnxsrv01.seas.ucla.edu>
2015-10-22 12:52:40 -0700

    Cleaned up execute command cde.

--- lab1a/execute-command.c
+++ lab1a/execute-command.c
@@ -106,51 +106,34 @@ subshell_propagate_helper(command_t c, char *input, char *output)
 
 
 void 
-subshell_propagate_io (command_t c, char *input, char *output, int *pipe_redirection)
+subshell_propagate_io (command_t c)
 {
-    command_t command_first = c;
-    command_t command_last = c;
+  char *input = c->input;
+  char *output = c->output;
+  int *pipe_redirection = c->pipe_redirection;
+  c=c->u.subshell_command;
+  command_t command_first = c;
+  command_t command_last = c;
 
-    while(1)
-    {
-      if (command_first->type == SIMPLE_COMMAND || command_first->type == SUBSHELL_COMMAND)
-      {
-        break;
-      }
-      else
-      {
-        command_first = command_first->u.command[0];
-      }
-    }
-
-    while(1)
-    {
-      if (command_last->type == SIMPLE_COMMAND || command_last->type == SUBSHELL_COMMAND)
-      {
-        break;
-      }
-      else
-      {
-        command_last = command_last->u.command[1];
-      }
-    }
+  while(command_first->type != SIMPLE_COMMAND && command_first->type != SUBSHELL_COMMAND) 
+    command_first = command_first->u.command[0];
+  while(command_last->type != SIMPLE_COMMAND && command_last->type != SUBSHELL_COMMAND)
+    command_last = command_last->u.command[1];
 
 
-    if (pipe_redirection[0] == 1 || pipe_redirection[0] == 3)
-    {
-      command_first->pipe_redirection[1] = pipe_redirection[1];
-      command_first->pipe_redirection[0] += 1;
-    }
+  if (pipe_redirection[0] == 1 || pipe_redirection[0] == 3)
+  {
+    command_first->pipe_redirection[1] = pipe_redirection[1];
+    command_first->pipe_redirection[0] += 1;
+  }
 
-    if (pipe_redirection[0] == 2 || pipe_redirection[0] == 3)
-    {
-      command_last->pipe_redirection[2] = pipe_redirection[2];
-      command_last->pipe_redirection[0] += 2;
-    }
+  if (pipe_redirection[0] == 2 || pipe_redirection[0] == 3)
+  {
+    command_last->pipe_redirection[2] = pipe_redirection[2];
+    command_last->pipe_redirection[0] += 2;
+  }
 
-    subshell_propagate_helper(c, input, output);
-
-
+  subshell_propagate_helper(c, input, output);
 }
 
 
@@ -160,90 +143,41 @@ pipe_connect (command_t c)
   command_t command_send = c->u.command[0];
   command_t command_get = c->u.command[1];
 
-//  int *fd = malloc(sizeof(int) * 2);
   int fd[2];
   pipe(fd);
 
+  while(command_send->type != SIMPLE_COMMAND && command_send->type != SUBSHELL_COMMAND)
+    command_send = command_send->u.command[1];
+  while(command_get->type != SIMPLE_COMMAND && command_get->type != SUBSHELL_COMMAND)
+    command_get = command_get->u.command[0];
 
-  while(1)
-  {
-    if (command_send->type == SIMPLE_COMMAND || command_send->type == SUBSHELL_COMMAND)
-    {
-      break;
-    }
-    else
-    {
-      command_send = command_send->u.command[1];
-    }
-  }
-
-  while(1)
-  {
-    if (command_get->type == SIMPLE_COMMAND || command_get->type == SUBSHELL_COMMAND)
-    {
-      break;
-    }
-    else
-    {
-      command_get = command_get->u.command[0];
-    }
-  }
-
-  /*
-      if (command_send->output == NULL && command_get->input == NULL)
-      {
-        command_send->pipe_redirection[0] += 2;
-        command_send->pipe_redirection[2] = fd[1];
-      }
-
-      if (command_get->input == NULL && command_send->output == NULL)
-      {
-        command_get->pipe_redirection[0] += 1;
-        command_get->pipe_redirection[1] = fd[0];
-      }
-      */
-        command_send->pipe_redirection[0] += 2;
-        command_send->pipe_redirection[2] = fd[1];
-        command_get->pipe_redirection[0] += 1;
-        command_get->pipe_redirection[1] = fd[0];
+  command_send->pipe_redirection[0] += 2;
+  command_send->pipe_redirection[2] = fd[1];
+  command_get->pipe_redirection[0] += 1;
+  command_get->pipe_redirection[1] = fd[0];
 }
 
 
 
 
 void
-pipe_propagate_io (command_t c, char *input, char *output)
+pipe_propagate_io (command_t c)
 {
   command_t command_first = c->u.command[0];
   command_t command_last = c->u.command[1];
+  char *input = c->input;
+  char *output = c->output;
 
-  while(1)
-  {
-    if (command_first->type == SIMPLE_COMMAND || command_first->type == SUBSHELL_COMMAND)
-    {
-      if (command_first->input == NULL)
-        command_first->input = input;
-      break;
-    }
-    else
-    {
-      command_first = command_first->u.command[0];
-    }
-  }
+  while(command_first->type != SIMPLE_COMMAND && command_first->type != SUBSHELL_COMMAND)
+    command_first = command_first->u.command[0];
 
-  while(1)
-  {
-    if (command_last->type == SIMPLE_COMMAND || command_last->type == SUBSHELL_COMMAND)
-    {
-      if (command_last->output == NULL)
-        command_last->output = output;
-      break;
-    }
-    else
-    {
-      command_last = command_last->u.command[1];
-    }
-  }
+  while(command_last->type != SIMPLE_COMMAND && command_last->type != SUBSHELL_COMMAND)
+    command_last = command_last->u.command[1];
+
+  if (command_first->input == NULL)
+    command_first->input = input;
+  if (command_last->output == NULL)
+    command_last->output = output;
 
 }
 
@@ -251,11 +185,6 @@ pipe_propagate_io (command_t c, char *input, char *output)
 void
 execute_command (command_t c, int time_travel)
 {
-  /* FIXME: Replace this with your implementation.  You may need to
-     add auxiliary functions and otherwise modify the source code.
-     You can also use external functions defined in the GNU C Library.  */
-
-
   switch(c->type)
   {
     case(SIMPLE_COMMAND):
@@ -302,7 +231,7 @@ execute_command (command_t c, int time_travel)
       //(cat <file1 && tr a z && cat) < file2
       //(echo lmao > file1 | tr h z) < file2
 
-      subshell_propagate_io(c->u.subshell_command, c->input, c->output, c->pipe_redirection);
+      subshell_propagate_io(c);
       execute_command(c->u.subshell_command, time_travel);
       c->status = c->u.subshell_command->status;
       
@@ -312,7 +241,7 @@ execute_command (command_t c, int time_travel)
       //interesting example
       //(echo hello | tr h z) 
       //(echo hello | tr h z < file1)
-      pipe_propagate_io(c, c->input, c->output);
+      pipe_propagate_io(c);
       pipe_connect(c);
       execute_command(c->u.command[0], time_travel);
       execute_command(c->u.command[1], time_travel);
