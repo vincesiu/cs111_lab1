874a94d91ae52abc843e7059067cd12b8a78ea4f
vince <siuv@lnxsrv04.seas.ucla.edu>
2015-11-05 18:40:26 -0800

    Fixed it so each fork only runs once! Now, I need to add dependency checking and it'll be all done.

--- lab1a/parallelizer.c
+++ lab1a/parallelizer.c
@@ -2,7 +2,8 @@
 #include "execute-command.h"
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <sys/types.h>
+#include <sys/wait.h>
 
 void parallel_execute(command_stream_t head)
 {
@@ -13,28 +14,62 @@ void parallel_execute(command_stream_t head)
 
   int num_running = 0;
   pid_t pid;
+  int status = 0;
+  int idx = 0;
 
   do 
   {
-    if (current->dependency_num == 0)
+    num_running = 0;
+    
+    for ( current = head; current != NULL; current = current->next)
     {
-      if ((pid = fork()) < 0)
+
+      if (!current->flag_commanddone && current->dependency_num == 0)
       {
-        error_parsing(0, "forking failed when trying to parallelize");
+        if ((pid = fork()) < 0)
+        {
+          error_parsing(0, "forking failed when trying to parallelize");
+        }
+        else if (pid == 0)
+        {
+          execute_command(current->tree, 0);
+          exit(current->tree->status);
+        }
+        else
+        {
+          current->flag_commandrunning = 1;
+          current->pid = pid;
+          num_running++;
+        }
       }
-      else if (pid == 0)
-      {
-        execute_command(current->tree, 0);
-        exit(EXIT_SUCCESS);
-      }
-      else
-      {
-        current->flag_commandrunning = 1;
-        current->pid = pid;
-        num_running++;
-      }
+
     }
-    current = current->next;
+
+    for ( idx = num_running; idx > 0; idx-- )
+    {
+      pid = waitpid( -1, &status, 0);
+
+      if (pid == -1)
+      {
+        error_parsing(current->tree->lineno, "waitpid returned unsuccessfully");
+      }
+
+      for ( current = head; current != NULL; current = current->next)
+      {
+        if (current->pid == pid)
+        {
+          current->flag_commandrunning = 0;
+          current->flag_commanddone = 1;
+          current->tree->status = status;
+
+          break;
+        }
+      }
+
+
+      
+    }
+
   } while (num_running != 0);
   
   return;
